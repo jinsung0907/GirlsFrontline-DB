49b71640fc7d04e58b8c0247e9035fba01fb9c7f
jinsung <jinsung@zzzzz.kr>
2018-06-08 01:18:56 +0900

    대문에 메인 이미지 표시하도록 설정

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
 	define("GF_HEADER", "aaaaa");
 	require_once("header.php");
+	
+	$dir = scandir("img/main/");
+	$dir = array_slice($dir, 2);
+	$img = $dir[rand(0, sizeof($dir)-1)];
 ?>
     <main role="main" class="container">
 		<div class="my-3 p-3 bg-white rounded box-shadow">
-			아대문만들기귀찮다
-			<br><br>
-			소녀전선 제조시간 카카오톡 봇 : <a href="https://pf.kakao.com/_VxgYxeC">링크</a><br>
-			<!-- 버그제보용 디스코드 : <a href="https://discord.gg/dGC3rFA">https://discord.gg/dGC3rFA</a>
-			<br><br>
-			<a href="http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2085633">http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2085633</a><br>
-			<a href="http://gall.dcinside.com/mgallery/board/view/?id=micateam&no=152506">http://gall.dcinside.com/mgallery/board/view/?id=micateam&no=152506</a><br>
-			<a href="http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2035070">http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2035070</a><br>
-			<a href="http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2035070">http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2035070</a><br>
-			<a href="http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2002838">http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=2002838</a><br>
-			<a href="http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=1954884">http://gall.dcinside.com/mgallery/board/view/?id=bjsn&no=1954884</a><br> -->
+			<img style="width:100%" src="img/main/<?=$img?>">
 		</div>
     </main>
 <?php
