8431865d2c16d8589eb960299afdd9da0d1eaa5b
jinsung <jinsung@zzzzz.kr>
2018-06-12 21:46:20 +0900

    live2d 새로 복호화 및 신규 추가

---
+++ furnitures.php
@@ -0,0 +1,14 @@
+<?php
+	define("GF_HEADER", "aaaaa");
+	require_once("header.php");
+?>
+    <main role="main" class="container">
+		<div class="my-3 p-3 bg-white rounded box-shadow">
+
+		</div>
+    </main>
+<?php
+	require_once("footer.php");
+?>
+	</body>
+</html>
