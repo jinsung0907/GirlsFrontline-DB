79569438d4e91e54b846cd2021cd2d5c15aa3844
jinsung <jinsung@zzzzz.kr>
2018-08-13 11:39:18 +0900

    최신브라우저 사용 권고 알림 추가

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
     <script src="dist/jquery.lazy.min.js"></script>
     <script src="dist/popper.min.js"></script>
     <script src="dist/bootstrap.min.js"></script>
+	<script src="dist/outdatedbrowser/outdatedbrowser.min.js"></script>
 	<script>
 		$(function () {
 			$('[data-toggle="tooltip"]').tooltip()
@@ -10,6 +11,15 @@
 		function sel_lang(val) {
 			window.location.href = 'http://<?=$_SERVER['HTTP_HOST']?>?lang=' + val;
 		}
+		
+		$( document ).ready(function() {
+			outdatedBrowser({
+				bgColor: '#f25648',
+				color: '#ffffff',
+				lowerThan: 'IE11',
+				languagePath: 'dist/outdatedbrowser/lang/<?=$lang?>.html'
+			})
+		})
 	</script>
 	<select onchange="sel_lang(this.value)">
 		<option>Languages</option>
@@ -18,3 +28,4 @@
 		<option value='ja'>Japanese</option>
 	</select>
 	<small>해당 사이트는 '소녀전선'의 데이터를 기반으로 작성되었으며, 그에 관련한 모든 저작권, 권리는 SUNBORN Network Technology, Digital Sky Entertainment, 哔哩哔哩, X.D. global에게 귀속되어있습니다.</small>
+	<div id="outdated"></div>
--- header.php
+++ header.php
@@ -34,6 +34,7 @@ if(!isset($header_image)) $header_image = ''; else $header_image = '<meta proper
 
     <link rel="stylesheet" href="dist/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<link rel="stylesheet" href="dist/outdatedbrowser/outdatedbrowser.min.css">
     <link rel="stylesheet" href="dist/gfdb.css?v=5">
   </head>
 
