0366b34e9b07d596770cba416ea02de02fdad200
jinsung <jinsung@zzzzz.kr>
2018-08-29 02:28:59 +0900

    개장스토리 자동화

--- substory_list.php
+++ substory_list.php
@@ -11,6 +11,32 @@
 		$storys = json_decode(file_get_contents("data/substory_$lang.json"));
 	}
 	else $storys = json_decode(file_get_contents("data/substory.json"));
+	
+	$dolls = json_decode(file_get_contents("data/doll.json"));
+	//live2d 리스트
+	$modstory = [];
+	if($lang != 'ko') 
+		$dir = "story_json/" . $lang . "/memoir";
+	else
+		$dir = "story_json/memoir";
+	
+	$dir = array_slice(scandir($dir), 2);
+	for($i = 0 ; $i < sizeof($dir) ; $i++) {
+		foreach($dolls as $doll) {
+			if(explode("_", $dir[$i])[0] == $doll->id) {
+				array_push($modstory, [getDollName($doll), $doll->id]);
+				$i += 3;
+				break;
+			}
+		}
+	}
+	if(sizeof($live2dlist) >= 1) {
+		$live2d_list = json_encode($live2dlist);
+	}
+	else {
+		$live2d_list = '\'\'';
+	}
+	
 ?>
     <main role="main" class="container">
 		<div class="my-3 p-3 bg-white rounded box-shadow">
@@ -18,117 +44,15 @@
 			<h6 class="border-bottom border-gray pb-2 mb-0"></h6>
 			<div class="media text-muted pt-3">
 				<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-					<strong class="d-block text-dark">콜트 리볼버</strong>
-					<a href="substory.php?t=1&q=1-1">MOD 1</a>
-					<a href="substory.php?t=1&q=1-2">MOD 2</a>
-					<a href="substory.php?t=1&q=1-3">MOD 3</a>
-					<a href="substory.php?t=1&q=1-4">MOD 4</a>
+		<?php
+				foreach($modstory as $mstory) { ?>
+					<strong class="d-block text-dark"><?=$mstory[0]?></strong>
+					<a href="substory.php?t=1&q=<?=$mstory[1]?>-1">MOD 1</a>
+					<a href="substory.php?t=1&q=<?=$mstory[1]?>-2">MOD 2</a>
+					<a href="substory.php?t=1&q=<?=$mstory[1]?>-3">MOD 3</a>
+					<a href="substory.php?t=1&q=<?=$mstory[1]?>-4">MOD 4</a>
 					<br><br>
-					
-					<strong class="d-block text-dark">M1911</strong>
-					<a href="substory.php?t=1&q=2-1">MOD 1</a>
-					<a href="substory.php?t=1&q=2-2">MOD 2</a>
-					<a href="substory.php?t=1&q=2-3">MOD 3</a>
-					<a href="substory.php?t=1&q=2-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">SV-98</strong>
-					<a href="substory.php?t=1&q=44-1">MOD 1</a>
-					<a href="substory.php?t=1&q=44-2">MOD 2</a>
-					<a href="substory.php?t=1&q=44-3">MOD 3</a>
-					<a href="substory.php?t=1&q=44-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">M4A1</strong>
-					<a href="substory.php?t=1&q=55-1">MOD 1</a>
-					<a href="substory.php?t=1&q=55-2">MOD 2</a>
-					<a href="substory.php?t=1&q=55-3">MOD 3</a>
-					<a href="substory.php?t=1&q=55-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">G3</strong>
-					<a href="substory.php?t=1&q=63-1">MOD 1</a>
-					<a href="substory.php?t=1&q=63-2">MOD 2</a>
-					<a href="substory.php?t=1&q=63-3">MOD 3</a>
-					<a href="substory.php?t=1&q=63-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">FN-49</strong>
-					<a href="substory.php?t=1&q=51-1">MOD 1</a>
-					<a href="substory.php?t=1&q=51-2">MOD 2</a>
-					<a href="substory.php?t=1&q=51-3">MOD 3</a>
-					<a href="substory.php?t=1&q=51-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">M4 SOPMOD II</strong>
-					<a href="substory.php?t=1&q=56-1">MOD 1</a>
-					<a href="substory.php?t=1&q=56-2">MOD 2</a>
-					<a href="substory.php?t=1&q=56-3">MOD 3</a>
-					<a href="substory.php?t=1&q=56-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">ST AR-15</strong>
-					<a href="substory.php?t=1&q=57-1">MOD 1</a>
-					<a href="substory.php?t=1&q=57-2">MOD 2</a>
-					<a href="substory.php?t=1&q=57-3">MOD 3</a>
-					<a href="substory.php?t=1&q=57-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">브렌</strong>
-					<a href="substory.php?t=1&q=89-1">MOD 1</a>
-					<a href="substory.php?t=1&q=89-2">MOD 2</a>
-					<a href="substory.php?t=1&q=89-3">MOD 3</a>
-					<a href="substory.php?t=1&q=89-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">IDW</strong>
-					<a href="substory.php?t=1&q=93-1">MOD 1</a>
-					<a href="substory.php?t=1&q=93-2">MOD 2</a>
-					<a href="substory.php?t=1&q=93-3">MOD 3</a>
-					<a href="substory.php?t=1&q=93-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">64식</strong>
-					<a href="substory.php?t=1&q=94-1">MOD 1</a>
-					<a href="substory.php?t=1&q=94-2">MOD 2</a>
-					<a href="substory.php?t=1&q=94-3">MOD 3</a>
-					<a href="substory.php?t=1&q=94-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">UMP45</strong>
-					<a href="substory.php?t=1&q=103-1">MOD 1</a>
-					<a href="substory.php?t=1&q=103-2">MOD 2</a>
-					<a href="substory.php?t=1&q=103-3">MOD 3</a>
-					<a href="substory.php?t=1&q=103-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">나강 리볼버</strong>
-					<a href="substory.php?t=1&q=5-1">MOD 1</a>
-					<a href="substory.php?t=1&q=5-2">MOD 2</a>
-					<a href="substory.php?t=1&q=5-3">MOD 3</a>
-					<a href="substory.php?t=1&q=5-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">모신나강</strong>
-					<a href="substory.php?t=1&q=39-1">MOD 1</a>
-					<a href="substory.php?t=1&q=39-2">MOD 2</a>
-					<a href="substory.php?t=1&q=39-3">MOD 3</a>
-					<a href="substory.php?t=1&q=39-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">브라우닝 M1918</strong>
-					<a href="substory.php?t=1&q=75-1">MOD 1</a>
-					<a href="substory.php?t=1&q=75-2">MOD 2</a>
-					<a href="substory.php?t=1&q=75-3">MOD 3</a>
-					<a href="substory.php?t=1&q=75-4">MOD 4</a>
-					<br><br>
-					
-					<strong class="d-block text-dark">MP-446</strong>
-					<a href="substory.php?t=1&q=91-1">MOD 1</a>
-					<a href="substory.php?t=1&q=91-2">MOD 2</a>
-					<a href="substory.php?t=1&q=91-3">MOD 3</a>
-					<a href="substory.php?t=1&q=91-4">MOD 4</a>
-					<br><br>
+		<?php } ?>
 				</p>
 			</div>
 		</div>
