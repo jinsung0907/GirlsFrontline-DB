18a2da6ca252c4caedf1062039d28c24444b3b8c
jinsung <jinsung@zzzzz.kr>
2018-09-08 01:24:56 +0900

    번역 추가

--- substory_list.php
+++ substory_list.php
@@ -40,7 +40,7 @@
 ?>
     <main role="main" class="container">
 		<div class="my-3 p-3 bg-white rounded box-shadow">
-			<h2 style="display: inline;margin-right:10px">인형의 추억 (개장스토리)</h2>
+			<h2 style="display: inline;margin-right:10px"><?=L::story_dollmemory?></h2>
 			<h6 class="border-bottom border-gray pb-2 mb-0"></h6>
 			<div class="media text-muted pt-3">
 				<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
