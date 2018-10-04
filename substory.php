f0b410083fbc6355d88957d88a961102f9b51e43
jinsung <jinsung@zzzzz.kr>
2018-10-04 18:25:36 +0900

    인형의 추억 제목에 mod표시

--- substory.php
+++ substory.php
@@ -16,7 +16,7 @@
 		$dolls = getJson('doll');
 		foreach($dolls as $doll) {
 			if($doll->id == $div[0]) {
-				$title = getDollName($doll) . " ". L::story_dollmemory;
+				$title = getDollName($doll) . " ". L::story_dollmemory . " - MOD" . $div[1];
 			}
 		}
 		$file = json_decode(file_get_contents("story_json/{$langdir}memoir/".$div[0]."_".$div[1].".txt"));
