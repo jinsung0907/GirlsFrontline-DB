83de564497e85fb9e2b1eba9e15ddefc3294c8ec
jinsung <jinsung@zzzzz.kr>
2018-10-13 01:27:54 +0900

    게시판 추가 (일단은 dc크롤링만 및 읽기만 지원)

---
+++ bbs.php
@@ -0,0 +1,42 @@
+<?php
+	define("GF_HEADER", "aaaaa");
+	require_once("common.php");
+	require_once("header.php");
+	require_once("dbconfig.php");
+    
+    if(ctype_digit($_GET['id'])) {
+        $id = $_GET['id'];
+    }
+    else {
+        exit("bad input");
+    }
+    $result = sql_query("select * from bbs where id = $id");
+    $row = mysqli_fetch_array($result);
+	
+	$nick_icon = '';
+	switch($row['idtype']) {
+		case 1: $nick_icon = "<img src='img/fix_nik.gif'>"; break;
+		case 2: $nick_icon = "<img src='img/nik.gif'>"; break;
+	}
+?>
+    <style>
+        .content img {
+            max-width: 100%;
+        }
+    </style>
+    <main role="main" class="container">
+		<div class="my-3 p-3 bg-white rounded box-shadow">
+			<h4><?=$row['title']?></h4><small>글쓴이 : <?=$row['name']?><?=$nick_icon?>&nbsp;&nbsp;&nbsp;날짜 : <?=date("m/d H:i", $row['date'])?><br><hr>
+            <span class="content">
+			<small>원본 : <u><a href="<?=$row['orgurl']?>"><?=$row['orgurl']?></a></u>
+            <?=$row['content']?>
+            </span>
+			<hr>
+		</div>
+    </main>
+    
+<?php
+	require_once("footer.php");
+?>
+	</body>
+</html>
