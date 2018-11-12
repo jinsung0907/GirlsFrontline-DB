81b0c6642ad387377cbac9b517121eeef61ff562
jinsung <jinsung@zzzzz.kr>
2018-11-12 10:53:53 +0900

    css버전

--- header.php
+++ header.php
@@ -37,7 +37,7 @@ if(!isset($header_keyword)) $header_keyword = '';
     <link rel="stylesheet" href="dist/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<link rel="stylesheet" href="dist/outdatedbrowser/outdatedbrowser.min.css">
-    <link rel="stylesheet" href="dist/gfdb.css?v=7">
+    <link rel="stylesheet" href="dist/gfdb.css?v=8">
   </head>
 
   <body class="bg-secondary">
