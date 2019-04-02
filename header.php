642ec725132ac733c7c194d29b94cb3e0d94685e
jinsung <jinsung@zzzzz.kr>
2019-04-03 00:40:04 +0900

    http -> https

--- header.php
+++ header.php
@@ -1,5 +1,10 @@
 <?php if(GF_HEADER != "aaaaa") exit; 
 
+if($_SERVER["HTTPS"] != "on") {
+  header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+  exit();
+}
+
 if(!isset($header_title)) $header_title = L::sitetitle_short . ' | zzzzz.kr';
 if(!isset($header_desc)) $header_desc = '소녀전선 데이터베이스 사이트입니다.';
 if(!isset($header_image)) $header_image = ''; else $header_image = '<meta property="og:image" content="' . $header_image . '"/>';
@@ -27,7 +32,7 @@ if(!isset($header_keyword)) $header_keyword = '';
 	<meta name="theme-color" content="#F0A900"> 
 	<meta property="og:type" content="website"/>
 	<meta property="og:title" content="<?=$header_title?>"/>
-	<meta property="og:url" content="http://<?=$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>"/>
+	<meta property="og:url" content="https://<?=$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>"/>
 	<?=$header_image?>
 	<meta property="og:description" content="<?=$header_desc?>"/>
 	<meta name="twitter:site" content="@gflkr"></meta>
